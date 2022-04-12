498d83047dac7980603cb67149479e397e158bd4
koneri <koneri13@gmail.com>
2022-04-12 13:29:26 +0400

    addedTwoWorksCommit

---
+++ src/main/kotlin/hw1.2.1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val amountInRubles = 500
+    val amount = amountInRubles * 100
+    val minFee = 3500
+
+    val fee = amount * 0.075
+    val result = if (fee < minFee) minFee.toDouble() else fee
+    println("комиссия равна ${result/100} рублей")
+}
